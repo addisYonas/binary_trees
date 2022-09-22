be098d3af37448a435b05a25a7f666c671208506
addisYonas <addyonas@gmail.com>
2022-09-22 13:43:29 -0700

    binary

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -34,8 +34,8 @@ size_t aux_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	height_left = aux_tree_height(tree->left) + 1;
-	height_right = aux_tree_height(tree->right) + 1;
+	height_left = binary_tree_height(tree->left) + 1;
+	height_right = binary_tree_height(tree->right) + 1;
 
 	if (height_left > height_right)
 		return (height_left);
